e74aec896f7fd22700d774a70aac321583828ed0
Alex <168570538+Alex-Mixaylov@users.noreply.github.com>
2025-12-08 01:44:21 +0300

    PostgreSQL DB
    PostgreSQL DB

--- abcp_tender_portal/abcp_tender_portal/settings.py
+++ abcp_tender_portal/abcp_tender_portal/settings.py
@@ -106,12 +106,35 @@ WSGI_APPLICATION = 'abcp_tender_portal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
+
+USE_POSTGRES = os.environ.get("DJANGO_USE_POSTGRES") == "1"
+
+if USE_POSTGRES:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.environ.get("POSTGRES_DB"),
+            "USER": os.environ.get("POSTGRES_USER"),
+            "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+            "HOST": os.environ.get("POSTGRES_HOST", "localhost"),
+            "PORT": os.environ.get("POSTGRES_PORT", "5432"),
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
+
 
 
 # Password validation
