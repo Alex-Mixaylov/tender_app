c72cec343faecc8aa20512235044baf15fd88fc7
Alex <168570538+Alex-Mixaylov@users.noreply.github.com>
2025-12-07 22:54:10 +0300

    update
    update

--- abcp_tender_portal/abcp_tender_portal/settings.py
+++ abcp_tender_portal/abcp_tender_portal/settings.py
@@ -25,7 +25,11 @@ MEDIA_ROOT = BASE_DIR / "media"
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nj*2z81r_h^%n0^#z*-q#d^6gbqk%+mw71%(f*m9q4e!-zt191'
+SECRET_KEY = os.environ.get(
+    "DJANGO_SECRET_KEY",
+    "django-insecure-nj*2z81r_h^%n0^#z*-q#d^6gbqk%+mw71%(f*m9q4e!-zt191"  # локальный dev-ключ
+)
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "False") == "True"
